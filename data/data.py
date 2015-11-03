088895bc927bbb4bface61c7da7933c2f73cbad7
Siyao Chang <changsiyao@berkeley.edu>
2015-11-02 21:21:45 -0800

    testing

--- data/data.py
+++ data/data.py
@@ -2,7 +2,10 @@ from __future__ import print_function, division
 
 import hashlib
 import os
-import json
+
+
+d = {'ds005_raw.tgz': "ab475fb09b300744548493394764f50e"}
+
 
 def generate_file_md5(filename, blocksize=2**20):
     m = hashlib.md5()
@@ -16,20 +19,16 @@ def generate_file_md5(filename, blocksize=2**20):
 
 
 def check_hashes(d):
-    #counter = 0
     all_good = True
     for k, v in d.items():
         digest = generate_file_md5(k)
         if v == digest:
-            #counter += 1
             print("The file {0} has the correct hash.".format(k))
         else:
             print("ERROR: The file {0} has the WRONG hash!".format(k))
             all_good = False
-    #print("There are " + str(counter) + " correct files.")
     return all_good
 
-d = json.load(open("data/hashList.txt"))
 
 if __name__ == "__main__":
     check_hashes(d)
