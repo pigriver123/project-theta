b24857e191bc0e1df4c0ca7d0abf38ce3ea79609
Siyao Chang <changsiyao@berkeley.edu>
2015-11-06 00:19:47 -0800

    update boundary option

--- code/findoutlier.py
+++ code/findoutlier.py
@@ -9,10 +9,14 @@ for i in range(1,10):
         # dvars path and name, call function to get outliers
         dvarsfile=txtpath+'dvars.txt'
         dvarssave=txtpath+'dvars_outlier_sub'+`i`+'run'+`j`+'.txt'
+        # common boundary for dvars is 0.3 - 0.5
+        # paper used 0.5
         dvars_outlier = outlier(dvarsfile,dvarssave, 0.5)
         # fd path and name, call function to get outliers
         fdfile=txtpath+'fd.txt'
         fdsave=txtpath+'fd_outlier_sub'+`i`+'run'+`j`+'.txt'
+        # common boundary for fd is 0.2 - 0.5
+        # paper used 0.5
         fd_outlier = outlier(fdfile, fdsave, 0.5)
 
 for i in range(10,17):
@@ -23,8 +27,12 @@ for i in range(10,17):
         # dvars path and name, call function to get outliers
         dvarsfile=txtpath+'dvars.txt'
         dvarssave=txtpath+'dvars_outlier_sub'+`i`+'run'+`j`+'.txt'
+        # common boundary for dvars is 0.3 - 0.5
+        # paper used 0.5
         dvars_outlier = outlier(dvarsfile,dvarssave, 0.5)
         # fd path and name, call function to get outliers
         fdfile=txtpath+'fd.txt'
         fdsave=txtpath+'fd_outlier_sub'+`i`+'run'+`j`+'.txt'
+        # common boundary for fd is 0.2 - 0.5
+        # paper used 0.5
         fd_outlier = outlier(fdfile, fdsave, 0.5)
