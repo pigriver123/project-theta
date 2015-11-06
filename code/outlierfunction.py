5ee52a1a15d84782db610454003d83aaf24b78a1
Siyao Chang <changsiyao@berkeley.edu>
2015-11-06 00:12:09 -0800

    add ing a boundary option for the function

--- code/outlierfunction.py
+++ code/outlierfunction.py
@@ -1,13 +1,13 @@
 
 # find outliers based on DVARS and FD
 
-def outlier(file_name, save_name):
+def outlier(file_name, save_name, bound):
     # load data
     data = np.loadtxt(file_name)
     outlier = []
     # set outlier values to 0
     for i in data:
-        if i <= 0.5:
+        if i <= bound:
             outlier.append(0)
         else:
             outlier.append(i)
