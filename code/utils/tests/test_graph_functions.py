43898a9143ab99322e768041f00dae42ea9a153c
Benjamin Hsieh <bhsieh@berkeley.edu>
2015-11-11 01:40:03 -0800

    attempt 2 to fix travis fail b/c of python version 3

--- code/utils/tests/test_graph_functions.py
+++ code/utils/tests/test_graph_functions.py
@@ -31,7 +31,7 @@ def test_loadtxt_dict():
     mydict = loadtxt_dict('temp.txt', 'mytxt')
     # True dictionary
     truedict = {'mytxt': np.arange(30.)}
-    assert all(mydict.values()[0] == truedict.values()[0])
+    assert_array_equal(list(mydict.values()), list(truedict.values()))
     assert_array_equal(mydict.keys(), truedict.keys())
 
 def test_vol_mean():
