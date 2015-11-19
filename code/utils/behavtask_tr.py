401ef1b70d14edc3f2aa65d25f80b7f7f3824c6e
Benjamin Hsieh <bhsieh@berkeley.edu>
2015-11-18 20:22:55 -0800

    merge all

--- code/utils/behavtask_tr.py
+++ code/utils/behavtask_tr.py
@@ -2,23 +2,28 @@ from __future__ import print_function, division
 import numpy as np
 import matplotlib.pyplot as plt
 
-def merge_cond(behav_cond, task_cond):
+def merge_cond(behav_cond, task_cond1, task_cond2, task_cond3, task_cond4):
     """
     Input: 
     behav_cond: txt file
     task_cond: txt file
     
-    Output: array 2d
+    Output: array 2d (86,11)
     Merged behav_task_cond
     An array of dim 2, merging the behav condition text file with the task 
-    conditions
+    conditions (4)
 
     """
     # Load the two text files
     behav = np.loadtxt(behav_cond, skiprows = 1)
-    task = np.loadtxt(task_cond)
-    # merge the two txt files
-    behav_task = np.insert(behav, [1],  task[:,1:], axis = 1)
+    task1 = np.loadtxt(task_cond1)
+    task2 = np.loadtxt(task_cond2)
+    task3 = np.loadtxt(task_cond3)
+    task4 = np.loadtxt(task_cond4)
+    # merge the txt files
+    # Appended task
+    total_task = np.append(task1[:,1:].T, task2[:,-1],task3[:,-1], task4[:,-1]).reshape(5,-1).T
+    behav_task = np.insert(behav, [1],  total_task, axis = 1)
     return behav_task
 
 def events2neural_extend(behav_task, tr, n_trs):
@@ -51,14 +56,17 @@ def events2neural_extend(behav_task, tr, n_trs):
     # Neural time course from onset, duration, amplitude, gain, loss, PTval, 
     # respnum,respcat, RT for each event
     time_course = np.zeros((n_trs, 7))
-    for on, dur, amp, gain, loss, PTval, respnum,respcat, RT in behav_task:
-        time_course[on:on + dur,0] = amp
-        time_course[on:on + dur,1] = gain
-        time_course[on:on + dur,2] = loss
-        time_course[on:on + dur,3] = PTval
-        time_course[on:on + dur,4] = respnum
-        time_course[on:on + dur,5] = respcat
-        time_course[on:on + dur,6] = RT
+    for on, dur, amp1, amp2, amp3, amp4, gain, loss, PTval, respnum,respcat, RT in behav_task:
+        time_course[on:on + dur,0] = amp1
+        time_course[on:on + dur,1] = amp2
+        time_course[on:on + dur,2] = amp3
+        time_course[on:on + dur,3] = amp4
+        time_course[on:on + dur,4] = gain
+        time_course[on:on + dur,5] = loss
+        time_course[on:on + dur,6] = PTval
+        time_course[on:on + dur,7] = respnum
+        time_course[on:on + dur,8] = respcat
+        time_course[on:on + dur,9] = RT
     return time_course
 
 def plot_time_course(time_course):
