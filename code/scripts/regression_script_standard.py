8f313fcac59a6c8ab28fcb884f1677cd42c6ee6f
Siyao Chang <changsiyao@berkeley.edu>
2015-12-13 01:29:24 -0800

    update regression scripts for new functions

--- code/scripts/regression_script_standard.py
+++ code/scripts/regression_script_standard.py
@@ -10,7 +10,7 @@ pathtofunction = '../utils/functions'
 sys.path.append(pathtofunction)
 
 from behavtask_tr import events2neural_extend, merge_cond
-from regression_functions_standard import hrf, getRegressor, calcBeta, calcMRSS, deleteOutliers
+from regression_functions import hrf, getRegressor, calcBeta, calcMRSS, deleteOutliers
 from smooth_gaussian import fwhm2sigma, smooth_spatial
 from calc_t import significant
 
@@ -46,13 +46,13 @@ for i in range(1,17):
         task_cond4 = 'ds005/sub0'+str(i).zfill(2)+'/model/model001/onsets/task001_run00'+`j`+'/cond004.txt'
         parameters = merge_cond(behav_cond, task_cond1, task_cond2, task_cond3, task_cond4)
         neural_prediction = events2neural_extend(parameters,TR, n_vols)
-        gain, loss = getRegressor(TR, n_vols, hrf_at_trs, neural_prediction)
+        gain, loss = getRegressor(TR, n_vols, hrf_at_trs, neural_prediction, standard = True)
         data, gain, loss = deleteOutliers(data, gain, loss, i, run, dvars_out, fd_out)
         data_full = np.concatenate((data_full,data),axis=3)
         gain_full = np.concatenate((gain_full,gain),axis=0)
         loss_full = np.concatenate((loss_full,loss),axis=0)
-    # mea=calcMRSS(data_full, gain_full, loss_full, threshold)
-    X, Y, beta=calcBeta(data_full, gain_full, loss_full, threshold)
+    # mea=calcMRSS(data_full, gain_full, loss_full, None, None, threshold)
+    X, Y, beta=calcBeta(data_full, gain_full, loss_full, None, None, threshold)
     # calculate t values
     t_val=np.zeros((2,139264))
     for k in range(Y.shape[1]):
