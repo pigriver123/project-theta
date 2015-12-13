81ed81bc3fcc8f0236518e0d9b3b2d8f83c7e916
Siyao Chang <changsiyao@berkeley.edu>
2015-12-12 17:52:37 -0800

    add plot heatmap script

--- code/scripts/regression_script_standard.py
+++ code/scripts/regression_script_standard.py
@@ -10,7 +10,7 @@ pathtofunction = '../utils/functions'
 sys.path.append(pathtofunction)
 
 from behavtask_tr import events2neural_extend, merge_cond
-from regression_functions import hrf, getRegressor, calcBeta, calcMRSS, deleteOutliers
+from regression_functions_standard import hrf, getRegressor, calcBeta, calcMRSS, deleteOutliers
 from smooth_gaussian import fwhm2sigma, smooth_spatial
 from calc_t import significant
 
@@ -23,20 +23,18 @@ os.chdir("../../data")
 
 dvars_out = json.load(open("../paper/data_results/dvarsOutliers.txt"))
 fd_out = json.load(open("../paper/data_results/fdOutliers.txt"))
+
 # Threshold cutoff from histogram observation
-threshold = 400
+threshold = 7500
 
 for i in range(1,17):
-    # first three dimension for data shape is 64, 64, 34.
+    # first three dimension for data shape is 91, 109, 91.
     # create array to store the combined dataset of three runs
-    data_full = np.empty([64, 64, 34, 0])
+    data_full = np.empty([91, 109, 91, 0])
     gain_full = np.empty([0,])
     loss_full = np.empty([0,])
-    linear_full = np.empty([0,])
-    quad_full = np.empty([0,])
     for j in range(1,4):
-        direct='ds005/sub0'+str(i).zfill(2)+'/BOLD/task001_run00'+`j`+'/'
-        boldname = direct+'bold.nii.gz'
+        boldname='ds005_mnifunc/sub0'+str(i).zfill(2)+'/model/model001/task001_run00'+`j`+'.feat/filtered_func_data_mni.nii.gz'
         img=nib.load(boldname)
         data=img.get_data()
         data=smooth_spatial(data)
@@ -48,22 +46,20 @@ for i in range(1,17):
         task_cond4 = 'ds005/sub0'+str(i).zfill(2)+'/model/model001/onsets/task001_run00'+`j`+'/cond004.txt'
         parameters = merge_cond(behav_cond, task_cond1, task_cond2, task_cond3, task_cond4)
         neural_prediction = events2neural_extend(parameters,TR, n_vols)
-        gain, loss, linear_dr, quad_dr = getRegressor(TR, n_vols, hrf_at_trs, neural_prediction)
-        data, gain, loss, linear_dr, quad_dr = deleteOutliers(data, gain, loss, linear_dr, quad_dr, i, run, dvars_out, fd_out)
+        gain, loss = getRegressor(TR, n_vols, hrf_at_trs, neural_prediction)
+        data, gain, loss = deleteOutliers(data, gain, loss, i, run, dvars_out, fd_out)
         data_full = np.concatenate((data_full,data),axis=3)
         gain_full = np.concatenate((gain_full,gain),axis=0)
         loss_full = np.concatenate((loss_full,loss),axis=0)
-        linear_full = np.concatenate((linear_full,linear_dr),axis=0)
-        quad_full = np.concatenate((quad_full,quad_dr),axis=0)
-    mea=calcMRSS(data_full, gain_full, loss_full, linear_full, quad_full, threshold)
-    X, Y, beta=calcBeta(data_full, gain_full, loss_full, linear_full, quad_full, threshold)
+    # mea=calcMRSS(data_full, gain_full, loss_full, threshold)
+    X, Y, beta=calcBeta(data_full, gain_full, loss_full, threshold)
     # calculate t values
     t_val=np.zeros((2,139264))
     for i in range(Y.shape[1]):
         t_val[:,i] = significant(X,Y[:,i], beta[:,i])
     # file names for beta and t
-    beta_file='../paper/data_results/sub0'+str(i).zfill(2)+'_beta.txt'
-    t_file='../paper/data_results/sub0'+str(i).zfill(2)+'_tvals.txt'
+    beta_file='../paper/data_results/sub0'+str(i).zfill(2)+'_standard_beta.txt'
+    t_file='../paper/data_results/sub0'+str(i).zfill(2)+'_standard_tvals.txt'
     # save beta and t values to file
     np.savetxt(beta_file, beta)
     np.savetxt(t_file, t_val)
